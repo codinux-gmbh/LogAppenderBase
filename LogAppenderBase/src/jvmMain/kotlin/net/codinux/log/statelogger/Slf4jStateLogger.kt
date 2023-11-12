97a0101507122ea84a07b173862bc32c375c1020
dankito <git@dankito.net>
2023-11-12 21:41:27 +0100

    Added Slf4jStateLogger

---
+++ LogAppenderBase/src/jvmMain/kotlin/net/codinux/log/statelogger/Slf4jStateLogger.kt
@@ -0,0 +1,26 @@
+package net.codinux.log.statelogger
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+open class Slf4jStateLogger(
+    protected open val log: Logger
+) : AppenderStateLogger {
+
+    constructor(loggerName: String) : this(LoggerFactory.getLogger(loggerName))
+
+    constructor(loggerClass: Class<*>) : this(LoggerFactory.getLogger(loggerClass))
+
+    override fun info(message: String) {
+        log.info(message)
+    }
+
+    override fun warn(message: String) {
+        log.warn(message)
+    }
+
+    override fun error(message: String, e: Throwable?) {
+        log.error(message, e)
+    }
+
+}
