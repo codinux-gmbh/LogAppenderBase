40e6a59ac7e2a164bcc8954cec3b4cfeb5cd81cf
dankito <git@dankito.net>
2025-05-14 01:37:26 +0200

    Extracted CertificateTrustManager

---
+++ kubernetes/CodinuxKubernetesInfoRetriever/src/main/kotlin/net/codinux/log/kubernetes/web/CertificateTrustManager.kt
@@ -0,0 +1,62 @@
+package net.codinux.log.kubernetes.web
+
+import net.codinux.log.statelogger.AppenderStateLogger
+import java.security.KeyStore
+import java.security.cert.Certificate
+import java.security.cert.CertificateFactory
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.TrustManagerFactory
+import javax.net.ssl.X509TrustManager
+
+object CertificateTrustManager {
+
+    // fallback trust store if Kube server certificate is not mounted to pod
+    val TrustAllCertificatesTrustManager = object : X509TrustManager {
+        override fun getAcceptedIssuers(): Array<X509Certificate?> = arrayOf()
+        override fun checkClientTrusted(certs: Array<X509Certificate?>?, authType: String?) { }
+        override fun checkServerTrusted(certs: Array<X509Certificate?>?, authType: String?) {
+            // trust all certificates
+        }
+    }
+
+
+    fun createTrustManagerForCertificate(certificateString: String?, stateLogger: AppenderStateLogger): TrustManager {
+        try {
+            if (certificateString != null) {
+                val certificateFactory = CertificateFactory.getInstance("X.509")
+                val certificate = certificateFactory.generateCertificate(certificateString.byteInputStream())
+
+                return createTrustManagerForCertificate(certificate)
+            }
+        } catch (e: Throwable) { // should never occur
+            stateLogger.error("Could not add Kubernetes server certificate to KeyStore: $e")
+        }
+
+        return TrustAllCertificatesTrustManager
+    }
+
+    private fun createTrustManagerForCertificate(certificate: Certificate): X509TrustManager {
+        val keyStore = KeyStore.getInstance(KeyStore.getDefaultType()).apply {
+            this.load(null) // not really needed
+
+            this.setCertificateEntry("KubeServer", certificate) // use a random name?
+        }
+
+        return createTrustManagerForKeyStore(keyStore)
+    }
+
+    private fun createTrustManagerForKeyStore(keyStore: KeyStore): X509TrustManager {
+        val trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm()).apply {
+            this.init(keyStore)
+        }
+
+        val sslContext = SSLContext.getInstance("TLS").apply {
+            this.init(null, trustManagerFactory.trustManagers, null)
+        }
+
+        return trustManagerFactory.trustManagers.filterIsInstance<X509TrustManager>().first()
+    }
+
+}
--- kubernetes/CodinuxKubernetesInfoRetriever/src/main/kotlin/net/codinux/log/kubernetes/web/KtorWebClient.kt
+++ kubernetes/CodinuxKubernetesInfoRetriever/src/main/kotlin/net/codinux/log/kubernetes/web/KtorWebClient.kt
@@ -10,14 +10,6 @@ import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.json.Json
 import net.codinux.log.statelogger.AppenderStateLogger
 import net.codinux.log.statelogger.StdOutStateLogger
-import java.security.KeyStore
-import java.security.cert.Certificate
-import java.security.cert.CertificateFactory
-import java.security.cert.X509Certificate
-import javax.net.ssl.SSLContext
-import javax.net.ssl.TrustManager
-import javax.net.ssl.TrustManagerFactory
-import javax.net.ssl.X509TrustManager
 
 open class KtorWebClient(
     accessToken: String,
@@ -33,7 +25,7 @@ open class KtorWebClient(
         }
         engine {
             https {
-                trustManager = createTrustManagerForCertificate(kubeApiCertificate)
+                trustManager = CertificateTrustManager.createTrustManagerForCertificate(kubeApiCertificate, stateLogger)
             }
         }
 
@@ -42,15 +34,6 @@ open class KtorWebClient(
         }
     }
 
-
-    // fallback trust store if Kube server certificate is not mounted to pod
-    private val trustAllCertificatesTrustManager = object : X509TrustManager {
-        override fun getAcceptedIssuers(): Array<X509Certificate?> = arrayOf()
-        override fun checkClientTrusted(certs: Array<X509Certificate?>?, authType: String?) { }
-        override fun checkServerTrusted(certs: Array<X509Certificate?>?, authType: String?) {
-            // trust all certificates
-        }
-    }
 
 
     override suspend fun get(url: String): WebClientResponse = try {
@@ -61,44 +44,6 @@ open class KtorWebClient(
         stateLogger.error("Could not retrieve $url", e)
 
         WebClientResponse(-1, null, e)
-    }
-
-
-    private fun createTrustManagerForCertificate(certificateString: String?): TrustManager {
-        try {
-            if (certificateString != null) {
-                val certificateFactory = CertificateFactory.getInstance("X.509")
-                val certificate = certificateFactory.generateCertificate(certificateString.byteInputStream())
-
-                return createTrustManagerForCertificate(certificate)
-            }
-        } catch (e: Throwable) { // should never occur
-            stateLogger.error("Could not add Kubernetes server certificate to KeyStore: $e")
-        }
-
-        return trustAllCertificatesTrustManager
-    }
-
-    private fun createTrustManagerForCertificate(certificate: Certificate): X509TrustManager {
-        val keyStore = KeyStore.getInstance(KeyStore.getDefaultType()).apply {
-            this.load(null) // not really needed
-
-            this.setCertificateEntry("KubeServer", certificate) // use a random name?
-        }
-
-        return createTrustManagerForKeyStore(keyStore)
-    }
-
-    private fun createTrustManagerForKeyStore(keyStore: KeyStore): X509TrustManager {
-        val trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm()).apply {
-            this.init(keyStore)
-        }
-
-        val sslContext = SSLContext.getInstance("TLS").apply {
-            this.init(null, trustManagerFactory.trustManagers, null)
-        }
-
-        return trustManagerFactory.trustManagers.filterIsInstance<X509TrustManager>().first()
     }
 
 }
