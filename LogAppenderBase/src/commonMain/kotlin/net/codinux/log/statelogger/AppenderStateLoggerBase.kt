9d5d9a66f90f32dd220e68ccaff90eb636a54113
dankito <git@dankito.net>
2025-05-23 07:17:42 +0200

    Added to log message how often this message has been suppressed during that timespan

--- LogAppenderBase/src/commonMain/kotlin/net/codinux/log/statelogger/AppenderStateLoggerBase.kt
+++ LogAppenderBase/src/commonMain/kotlin/net/codinux/log/statelogger/AppenderStateLoggerBase.kt
@@ -1,5 +1,6 @@
 package net.codinux.log.statelogger
 
+import net.codinux.kotlin.concurrent.atomic.AtomicInt
 import net.codinux.kotlin.concurrent.collections.ConcurrentMap
 import net.dankito.datetime.Instant
 import kotlin.time.Duration
@@ -8,19 +9,26 @@ abstract class AppenderStateLoggerBase : AppenderStateLogger {
 
     protected open val lastErrorCalls = ConcurrentMap<String, Instant>()
 
+    protected open val countSuppressedCalls = ConcurrentMap<String, AtomicInt>()
+
 
     override fun error(message: String, e: Throwable?, logAtMaximumEach: Duration, category: String, addDurationToLogMessage: Boolean) {
         val lastErrorCall = lastErrorCalls[category]
 
         if (lastErrorCall == null || minimumTimeElapsed(lastErrorCall, logAtMaximumEach)) {
             lastErrorCalls[category] = Instant.now()
+            val suppressedCalls = countSuppressedCalls[category]?.get()
+            countSuppressedCalls[category] = AtomicInt(0)
 
             if (addDurationToLogMessage) {
                 // TODO: later convert Duration to a nicer string, e.g. "5m" -> "5 min"
-                error("$message This message is logged only once every $logAtMaximumEach.", e)
+                error("$message This message is logged only once every $logAtMaximumEach" +
+                        (if (suppressedCalls == null) "." else " and has been suppressed $suppressedCalls times during that time."), e)
             } else {
                 error(message, e)
             }
+        } else {
+            countSuppressedCalls[category]?.incrementAndGet()
         }
     }
 
